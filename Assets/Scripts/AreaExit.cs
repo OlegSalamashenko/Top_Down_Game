96299845ed24021eb02ca5739bc07da0659dec82
OlegSalamashenko <olegpups901@gmail.com>
2024-07-13 15:39:27 +0300

    4:23

--- Assets/Scripts/AreaExit.cs
+++ Assets/Scripts/AreaExit.cs
@@ -6,12 +6,25 @@ public class AreaExit : MonoBehaviour
 {
     [SerializeField] private string sceneToLoad;
     [SerializeField] private string scenetransitionName;
+
+    private float waitToLoadTime = 1f;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.GetComponent<PlayerController>())
         {
-            SceneManager.LoadScene(sceneToLoad);
             SceneManagement.Instance.SetTransitionName(scenetransitionName);
+            UIFade.Instance.FadeToBlack();
+            StartCoroutine(LoadSceneRotine());
         }
+    }
+
+    private IEnumerator LoadSceneRotine()
+    { 
+        while (waitToLoadTime >= 0)
+        {
+            waitToLoadTime -= Time.deltaTime;
+            yield return null;
+        }
+        SceneManager.LoadScene(sceneToLoad);
     }
 }
