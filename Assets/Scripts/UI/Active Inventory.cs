31582f5177077e0bcb4ded8994994b88bf96484d
OlegSalamashenko <olegpups901@gmail.com>
2024-07-15 17:03:46 +0300

    22

---
+++ Assets/Scripts/UI/Active Inventory.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveInventory : MonoBehaviour
+{
+    private int activeSlotIndexNum = 0;
+
+    private PlayerControls playerControls;
+
+    private void Awake()
+    {
+        playerControls = new PlayerControls();
+    }
+    private void Start()
+    {
+        playerControls.Inventory.Keyboard.performed += ctx => ToggleActiveSlot((int)ctx.ReadValue<float>());
+    }
+
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
+
+    private void ToggleActiveSlot(int numValue)
+    {
+        ToggleAtiveHighlight(numValue - 1);
+    }
+
+    private void ToggleAtiveHighlight(int indexNum)
+    { 
+        activeSlotIndexNum = indexNum;
+
+        foreach (Transform inventorySlot in this.transform)
+        {
+            inventorySlot.GetChild(0).gameObject.SetActive(false);
+        }
+
+        this.transform.GetChild(activeSlotIndexNum/*md problems activeSlotIndexNum = indexNum */).GetChild(0).gameObject.SetActive(true);
+    }
+}
