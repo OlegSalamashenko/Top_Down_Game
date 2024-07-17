495de1fccf1f51b071011109f7811d8c1479992d
OlegSalamashenko <olegpups901@gmail.com>
2024-07-17 21:48:41 +0300

    28

--- Assets/Scripts/UI/Bow.cs
+++ Assets/Scripts/UI/Bow.cs
@@ -5,13 +5,46 @@ using UnityEngine;
 public class Bow : MonoBehaviour , IWeapon
 {
     [SerializeField] private WeaponInfo weaponInfo;
+    [SerializeField] private GameObject arrowPrefab;
+    [SerializeField] private Transform arrowSpawnPoint;
+
+    readonly int FIRE_HASH = Animator.StringToHash("Fire");
+
+    private Animator myAnimator;
+
+    private void Awake()
+    {
+        myAnimator = GetComponent<Animator>();
+    }
+    private void Update()       //
+    {
+        MouseFollowWithOffset();
+    }
     public void Attack()
     {
-        Debug.Log("Bow Attack");
+        myAnimator.SetTrigger(FIRE_HASH);
+        GameObject newArrow = Instantiate(arrowPrefab,arrowSpawnPoint.position,ActiveWeapon.Instance.transform.rotation);
     }
     public WeaponInfo GetWeaponInfo()
     {
         return weaponInfo;
     }
-   
+    private void MouseFollowWithOffset()    //
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(PlayerController.Instance.transform.position);
+
+        Vector3 direction = mousePos - playerScreenPoint;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+
+        if (PlayerController.Instance.FacingLeft)
+        {
+            ActiveWeapon.Instance.transform.rotation = Quaternion.Euler(0, -180f, -angle + 180);
+        }
+        else
+        {
+            ActiveWeapon.Instance.transform.rotation = Quaternion.Euler(0, 0, angle);
+        }
+    }
 }
--- Assets/Scripts/UI/Staff.cs
+++ Assets/Scripts/UI/Staff.cs
@@ -14,10 +14,7 @@ public class Staff : MonoBehaviour , IWeapon
         Debug.Log("Staff Attack");
     }
 
-    public WeaponInfo GetWeaponInfo()
-    {
-        return weaponInfo;
-    }
+    
     private void MouseFollowWithOffset()
     {
         Vector3 mousePos = Input.mousePosition;
@@ -35,6 +32,12 @@ public class Staff : MonoBehaviour , IWeapon
         {
            ActiveWeapon.Instance.transform.rotation = Quaternion.Euler(0, 0, angle);
         }
+    }
+
+
+    public WeaponInfo GetWeaponInfo()
+    {
+        return weaponInfo;
     }
 
 }
