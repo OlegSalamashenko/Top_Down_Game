731dc43d0443d1c23a0a1903dcb2b7cce0e582f6
OlegSalamashenko <olegpups901@gmail.com>
2024-07-18 17:58:54 +0300

    30

--- Assets/Scripts/UI/Staff.cs
+++ Assets/Scripts/UI/Staff.cs
@@ -5,16 +5,35 @@ using UnityEngine;
 public class Staff : MonoBehaviour , IWeapon
 {
     [SerializeField] private WeaponInfo weaponInfo;
+    [SerializeField] private GameObject magicLaser;
+    [SerializeField] private Transform magicLaserSpawnPoint;
+
+    private Animator myAnimator;
+
+    readonly int AttackHash = Animator.StringToHash("Attack");
+
+    private void Awake()
+    {
+        myAnimator = GetComponent<Animator>();
+    }
     private void Update()
     {
         MouseFollowWithOffset();
     }
     public void Attack()
     {
-        Debug.Log("Staff Attack");
+        myAnimator.SetTrigger(AttackHash);
     }
 
-    
+    public void SpawnStaffProjectileAnimEvent()
+    {
+        GameObject newLaser = Instantiate(magicLaser,magicLaserSpawnPoint.position,Quaternion.identity);
+    }
+
+    public WeaponInfo GetWeaponInfo()
+    {
+        return weaponInfo;
+    }
     private void MouseFollowWithOffset()
     {
         Vector3 mousePos = Input.mousePosition;
@@ -35,9 +54,6 @@ public class Staff : MonoBehaviour , IWeapon
     }
 
 
-    public WeaponInfo GetWeaponInfo()
-    {
-        return weaponInfo;
-    }
+    
 
 }
