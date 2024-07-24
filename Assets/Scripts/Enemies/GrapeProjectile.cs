11bb29928a55c6260cc7b7f4f4e598505991f316
OlegSalamashenko <olegpups901@gmail.com>
2024-07-24 13:30:10 +0300

    49

--- Assets/Scripts/Enemies/GrapeProjectile.cs
+++ Assets/Scripts/Enemies/GrapeProjectile.cs
@@ -9,6 +9,7 @@ public class GrapeProjectile : MonoBehaviour
     [SerializeField] private AnimationCurve animCurve;
     [SerializeField] private float heightY = 3f;
     [SerializeField] private GameObject grapePtojectileShadow;
+    [SerializeField] private GameObject splatterPrefab;
 
     private void Start()
     {
@@ -38,7 +39,7 @@ public class GrapeProjectile : MonoBehaviour
 
             yield return null;
         }
-
+        Instantiate(splatterPrefab,transform.position,Quaternion.identity);
         Destroy(gameObject);
     }
 
