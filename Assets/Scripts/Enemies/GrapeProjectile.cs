962883835838e55c47d8004a45dafa5ecbcc7455
OlegSalamashenko <olegpups901@gmail.com>
2024-07-24 13:17:58 +0300

    48

---
+++ Assets/Scripts/Enemies/GrapeProjectile.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Animations;
+using UnityEngine;
+
+public class GrapeProjectile : MonoBehaviour
+{
+    [SerializeField] private float duration = 1f;
+    [SerializeField] private AnimationCurve animCurve;
+    [SerializeField] private float heightY = 3f;
+    [SerializeField] private GameObject grapePtojectileShadow;
+
+    private void Start()
+    {
+        GameObject grapeShadow =
+        Instantiate(grapePtojectileShadow,transform.position + new Vector3(0,-0.3f,0), Quaternion.identity);
+
+        Vector3 playerPos = PlayerController.Instance.transform.position;
+        Vector3 grapeShadowStartPosition = grapeShadow.transform.position;
+
+        StartCoroutine(projectileCurveRoutine(transform.position , playerPos));
+        StartCoroutine(MoveGrapeShadowRoutine(grapeShadow, grapeShadowStartPosition , playerPos));
+    }
+
+    private IEnumerator projectileCurveRoutine(Vector3 startPosition, Vector3 endPosition)
+    {
+        float timePassed = 0f;
+
+        while (timePassed < duration)
+        {
+            timePassed += Time.deltaTime;
+
+            float linearT = timePassed / duration;
+            float heightT = animCurve.Evaluate(linearT);
+            float height = Mathf.Lerp(0f,heightY,heightT);
+
+            transform.position = Vector2.Lerp(startPosition, endPosition, linearT) + new Vector2(0f,height);
+
+            yield return null;
+        }
+
+        Destroy(gameObject);
+    }
+
+    private IEnumerator MoveGrapeShadowRoutine(GameObject grapeShadow, Vector3 startposition, Vector3 endPosition)
+    {
+        float timePassed = 0f;
+
+        while (timePassed < duration) 
+        {
+            timePassed += Time.deltaTime;
+            float linearT = timePassed / duration;
+            grapeShadow.transform.position = Vector2.Lerp(startposition,endPosition,linearT);
+            yield return null;
+        }
+
+        Destroy(grapeShadow);
+    }
+}
